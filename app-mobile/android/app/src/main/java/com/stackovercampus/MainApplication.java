2649330f825fdda5180fd936c4ffb2184ea10d35
guilhermeadsf <guilherme.adsf@hotmail.com>
2019-11-24 20:56:29 -0300

    Configurando app center

--- app-mobile/android/app/src/main/java/com/stackovercampus/MainApplication.java
+++ app-mobile/android/app/src/main/java/com/stackovercampus/MainApplication.java
@@ -3,6 +3,9 @@ package com.stackovercampus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.rnfs.RNFSPackage;
 import io.github.elyx0.reactnativedocumentpicker.DocumentPickerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -31,6 +34,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new RNFSPackage(),
             new DocumentPickerPackage(),
             new ImagePickerPackage(),
